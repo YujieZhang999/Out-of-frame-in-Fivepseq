4d1fcadfc42e80951ee6a4bc41ec7e79f520d59f
Yujie Zhang <zhangyu1500@gmail.com>
2023-04-16 15:28:37 +0200

    Create preprocess_Fivepseq_yeast.sh

---
+++ Preprocessing/preprocess_Fivepseq_yeast.sh
@@ -0,0 +1,32 @@
+#!/bin/bash -l
+
+f_dir='fastq'
+fname=${f_dir##*/}
+
+echo ""
+echo "$f_dir"
+echo "$fname"
+
+export projectdir=/project/Fivepseq
+export refdir=$projectdir/genome/SC
+
+
+/proj/snic2019-30-56/lilit/prefivepseq/prefivepseq_yeast.sh \
+    -f $f_dir \
+    -i $refdir/star_index \
+    -o preprocess/$fname \
+    -a $refdir/gff/* \
+    -g $refdir/fa/* \
+    > log/$fname.log \
+
+
+
+fivepseq \
+    -g $refdir/fa/* \
+    -a refdir/gff/* \
+    --conflicts add \
+    -b ../preprocess/fastq/align_dedup/'*R1_001.fastqAligned.primary.out_dedup.bam' \
+    -o ../fivepseq/ \
+    -t FS \
+    -queue 30 \
+    --oof
